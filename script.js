bd3639b12fac2f493fade71eac536ceb701e0d07
Juan <jbellevue83022@gmail.com>
2024-11-09 02:16:51 -0600

    Added image loading

---
+++ script.js
@@ -0,0 +1,3 @@
+<img src="png-others/odin-lined.png" loading="lazy" alt="description"></img>;
+
+<img src="png-others/lexi-laginess-amTPFVMfIt0-unsplash.jpg" loading="lazy" alt="description"></img>;
